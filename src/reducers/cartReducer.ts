b47595add409aa8b16775871a7d1cfe0b9f47f78
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-05-21 22:00:45 +0200

    create cart reducer

---
+++ src/reducers/cartReducer.ts
@@ -0,0 +1,29 @@
+import {
+    CartState,
+    CartActionTypes,
+    ADD_TO_CART,
+} from '../types/types'
+
+const initialState: CartState = {
+    cart: []
+}
+
+export function cartReducer(
+    state = initialState,
+    action: CartActionTypes
+): CartState {
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+                cart: [...state.cart, action.payload]
+            }
+        // case DELETE_FROM_CART:
+        //     return {
+        //         cart: state.messages.filter(
+        //             // TODO
+        //         )
+        //     }
+        default:
+            return state
+    }
+}
