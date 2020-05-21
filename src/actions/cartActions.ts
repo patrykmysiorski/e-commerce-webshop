d059f8686baa3dcf881e398819929a06a6700032
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-05-21 22:00:24 +0200

    create cart actions

---
+++ src/actions/cartActions.ts
@@ -0,0 +1,15 @@
+import { Item, ADD_TO_CART, DELETE_FROM_CART, CartActionTypes } from '../types/types'
+
+export function addToCart(item: Item): CartActionTypes {
+    return {
+        type: ADD_TO_CART,
+        payload: item
+    }
+}
+
+export function deleteMessage(item: Item): CartActionTypes {
+    return {
+        type: DELETE_FROM_CART,
+        payload: item
+    }
+}
