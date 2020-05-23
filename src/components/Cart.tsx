6a702b8f047305a6444c032119296e128832b0fb
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-05-23 23:58:11 +0200

    delete bootstrap list, add buy form

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,25 +1,89 @@
 import React from 'react';
-import {ListGroup} from "react-bootstrap";
-import {Item} from "../types/types";
-import {useSelector} from "react-redux";
-import {RootState} from "../reducers";
+import { useSelector } from "react-redux";
+import { RootState } from "../reducers";
+import { Field, reduxForm } from 'redux-form'
+import '../css/cart.css'
 
-const Cart: React.FC = () => {
-
+const Cart: React.FC = (props: any) => {
     const items: any = useSelector<RootState>(state => state.cartReducer.cart)
-
-    console.log(items)
+    const {handleSubmit, pristine, reset, submitting} = props
 
     return (
         <div className="center">
             {items.map((listItem: any) => {
-                return (<ListGroup horizontal>
-                    <ListGroup.Item variant={'primary'}>{listItem['name']}</ListGroup.Item>
-                    <ListGroup.Item variant={'primary'}>{listItem['price']} EUR</ListGroup.Item>
-                </ListGroup>)
+                return (
+                    <tr>
+                        <td>{listItem['name']}</td>
+                        <td>{listItem['price']}</td>
+                    </tr>
+            )
             })}
+            <div className={'form'}>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>First Name</label>
+                        <div>
+                            <Field
+                                name="firstName"
+                                component="input"
+                                type="text"
+                                placeholder="First Name"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label>Last Name</label>
+                        <div>
+                            <Field
+                                name="lastName"
+                                component="input"
+                                type="text"
+                                placeholder="Last Name"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <div>
+                            <Field
+                                name="email"
+                                component="input"
+                                type="email"
+                                placeholder="Email"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="employed">Employed</label>
+                        <div>
+                            <Field
+                                name="employed"
+                                id="employed"
+                                component="input"
+                                type="checkbox"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label>Notes</label>
+                        <div>
+                            <Field name="notes" component="textarea"/>
+                        </div>
+                    </div>
+                    <div>
+                        <button type="submit" disabled={pristine || submitting}>
+                            Submit
+                        </button>
+                        <button type="button" disabled={pristine || submitting} onClick={reset}>
+                            Clear Values
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
 
-export default Cart;
+export default reduxForm({
+    form: 'customer'
+})(Cart);
