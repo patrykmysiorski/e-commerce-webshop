dcd235f2e1e70d932272867bfc9e94e24af0a3ff
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 00:50:55 +0200

    create component

---
+++ src/components/admin/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Menu from "./Menu";
+import '../../css/adminMenu.css'
+
+const Dashboard: React.FC = () =>
+    <div className="menu">
+        <Menu />
+    </div>
+
+export default Dashboard;
---
+++ src/components/admin/Menu.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { ADMIN_ROUTING } from "../../constants/adminRouting";
+import Notfound from "../NotFound";
+import '../../css/adminMenu.css'
+
+const Menu: React.FC = () =>
+    <div className="flex-container-row">
+        <Router>
+            <div className="flex-container">
+                {ADMIN_ROUTING.map(routeItem => {
+                    return (
+                        <div className={"margin"}>
+                            <Link to={routeItem.linkTo}>
+                                <button>
+                                    <span>{routeItem.buttonName}</span>
+                                </button>
+                            </Link>
+                        </div>)
+                })}
+            </div>
+            <Switch>
+                {ADMIN_ROUTING.map(routeItem => (
+                    <Route
+                        key={routeItem.linkTo}
+                        exact
+                        path={routeItem.linkTo}
+                        component={routeItem.component}
+                    />
+                ))}
+                <Route component={Notfound}/>
+            </Switch>
+        </Router>
+    </div>
+
+export default Menu;
