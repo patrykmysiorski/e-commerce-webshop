f50cf44d2c864c27880433b4ed7fcb83c7172dde
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 15:37:59 +0200

    change argument type

--- src/actions/productActions.ts
+++ src/actions/productActions.ts
@@ -1,14 +1,13 @@
 import { ADD_PRODUCT, DELETE_PRODUCT, Product, ProductActionTypes } from '../types/productsTypes'
 
 export function addProduct(product: Product): ProductActionTypes {
-    console.log('add product called!')
     return {
         type: ADD_PRODUCT,
         payload: product
     }
 }
 
-export function deleteProduct(id: string): ProductActionTypes {
+export function deleteProduct(id: number): ProductActionTypes {
     return {
         type: DELETE_PRODUCT,
         payload: id
