b95ffbe15ddf61262df9eb92f1c91d8520bfde83
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 17:32:23 +0200

    style home page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../css/standardFont.css'
+import '../css/app.css'
 
 const App: React.FC = () =>
-    <div className="App font-white">
-      <h1>App works!</h1>
+    <div className={"main-page"}>
+        <div>
+            <div className={"m-b-15"}>
+                <h1>Welcome to the e-commerce web shop.<br/>Say hello to the best games at the best prices.</h1>
+            </div>
+            <a className="btn btn-full" href="/products">Show me your games!</a>
+            <a className="btn btn-ghost" href="/contact">Why us?</a>
+        </div>
+
     </div>
 
 export default App;
