c2d183392117f262665921f6042363564aa4de73
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 15:41:45 +0200

    add white font

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import '../css/standardFont.css'
 
 const NotFound: React.FC = () =>
-    <h1>Page not found like trophies in Tottenham Hotspur museum :)!</h1>
+    <div className={"font-white"}>
+        <h1>Page not found like trophies in Tottenham Hotspur museum :)!</h1>
+    </div>
 
 export default NotFound;
