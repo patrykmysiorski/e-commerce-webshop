0eb73b365372b2cfc2ab33f37f638a1b552c21a1
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 23:49:50 +0200

    change text

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () =>
     <div className={"main-page"}>
         <div>
             <div className={"m-b-15"}>
-                <h1>Welcome to the e-commerce web shop.<br/>Say hello to the best games at the best prices.</h1>
+                <h1>Welcome to the e-commerce online store.<br/>Say hello to the best games at the best prices.</h1>
             </div>
             <a className="btn btn-full" href="/products">Show me your games!</a>
             <a className="btn btn-ghost" href="/contact">Why us?</a>
