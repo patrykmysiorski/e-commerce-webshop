e340e740f5a942dcd049443d16449fb15758b106
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 15:43:17 +0200

    create add product form

---
+++ src/components/admin/AddProductForm.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Field, getFormValues, reduxForm } from "redux-form";
+import { addProduct } from "../../actions/productActions";
+import { connect, useDispatch } from "react-redux";
+
+const AddProductForm: React.FC = (props: any) => {
+    const {pristine, reset, submitting} = props
+    const dispatch = useDispatch();
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        dispatch(addProduct(props.formValues));
+    }
+    return (
+        <div className="product-form">
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>title</label>
+                    <div>
+                        <Field
+                            name="title"
+                            component="input"
+                            type="text"
+                            placeholder="title"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label>thumbnail</label>
+                    <div>
+                        <Field
+                            name="thumbnail"
+                            component="input"
+                            type="text"
+                            placeholder="thumbnail"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label>category</label>
+                    <div>
+                        <Field
+                            name="category"
+                            component="input"
+                            type="text"
+                            placeholder="category"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label>price</label>
+                    <div>
+                        <Field
+                            name="price"
+                            component="input"
+                            type="number"
+                            placeholder="price"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        // disabled={pristine || submitting}
+                    >
+                        Add product!
+                    </button>
+                    <button type="button" disabled={pristine || submitting} onClick={reset}>
+                        Clear Values
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+const mapStateToProps = (state: any) => {
+    return {
+        formValues: getFormValues("addProduct")(state)
+    };
+};
+
+const withRedux = reduxForm({
+    form: 'addProduct',
+    initialValues: {
+        price: 15,
+        title: 'fifa',
+        category: 'sport',
+        thumbnail: 'https://images.morele.net/i1064/6315778_0_i1064.jpg'
+    }
+})(AddProductForm);
+
+export default connect(mapStateToProps)(withRedux)
