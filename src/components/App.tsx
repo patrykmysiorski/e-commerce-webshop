9b1d8ddfb480e88fb157b9d2c7d49b09c23f77ee
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 15:38:12 +0200

    add white font

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../css/standardFont.css'
 
 const App: React.FC = () =>
-    <div className="App">
+    <div className="App font-white">
       <h1>App works!</h1>
     </div>
 
