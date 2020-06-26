1f692fa0407fb90b846832a1448ef2b03bea804d
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 15:42:22 +0200

    add download games form git

--- src/components/RoutingList.tsx
+++ src/components/RoutingList.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Notfound from './NotFound';
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { ROUTING } from '../constants/routing'
 import '../css/routingList.css'
+import axios from "axios";
+import { addProduct } from "../actions/productActions";
+import { useDispatch } from "react-redux";
 
 const RoutingList = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        axios.get(`https://raw.githubusercontent.com/patrykmysiorski/e-commerce-webshop/master/jsons/games.json`)
+            .then(res => {
+                const games = res.data.games;
+                games.map((game: any, i: number) => {
+                    const gameWithId = {id: i, ...game}
+                    dispatch(addProduct(gameWithId));
+                })
+            })
+    }, []);
     return (
         <Router>
             <div className={"content"}>
