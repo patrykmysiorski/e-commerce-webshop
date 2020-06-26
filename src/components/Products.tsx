20e5e53a7946e2f87e7ea8ebdf238f144f98c3da
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 15:42:00 +0200

    wip

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import React from 'react'
 import { ListGroup } from 'react-bootstrap';
 import '../index.css'
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../actions/cartActions";
 import { Item } from "../types/cartTypes";
-import { addProduct } from "../actions/productActions";
 import { Product } from "../types/productsTypes";
 
 const Products: React.FC = () => {
     const games: Product[] = useSelector((state: any) => state.productsReducer.products)
     const dispatch = useDispatch()
-
-    useEffect(() => {
-        axios.get(`https://raw.githubusercontent.com/patrykmysiorski/e-commerce-webshop/master/jsons/games.json`)
-            .then(res => {
-                const games = res.data.games;
-                games.map((game: any, i: number) => {
-                    const gameWithId = {id: i, ...game}
-                    dispatch(addProduct(gameWithId));
-                })
-            })
-    }, []);
 
     const onButtonClick = (item: Item) => {
         dispatch(addToCart({
@@ -34,15 +21,15 @@ const Products: React.FC = () => {
         <div className="center">
             {games.map(listItem => {
                 const item: Item = {
-                    name: listItem['name'],
-                    price: listItem['price']
+                    name: listItem.title,
+                    price: listItem.price
                 }
-                return (<ListGroup horizontal>
-                    <ListGroup.Item variant={'primary'}>{listItem['name']}</ListGroup.Item>
-                    <ListGroup.Item variant={'primary'}>{listItem['category']}</ListGroup.Item>
-                    <ListGroup.Item variant={'primary'}>{listItem['price']} EUR</ListGroup.Item>
-                    <ListGroup.Item variant={'primary'}><img src={listItem['image']} width="100px"
-                                                             height="100px" alt={listItem['name']}/></ListGroup.Item>
+                return (<ListGroup horizontal key={listItem.id}>
+                    <ListGroup.Item variant={'primary'}>{listItem.title}</ListGroup.Item>
+                    <ListGroup.Item variant={'primary'}>{listItem.category}</ListGroup.Item>
+                    <ListGroup.Item variant={'primary'}>{listItem.price} EUR</ListGroup.Item>
+                    <ListGroup.Item variant={'primary'}><img src={listItem.thumbnail} width="100px"
+                                                             height="100px" alt={listItem.title}/></ListGroup.Item>
                     <ListGroup.Item variant={'primary'}>
                         <button type="button" className="btn btn-primary" onClick={() => onButtonClick(item)}>Add to
                             cart
