5965dceef975dc864a966922f9cae47e320da09e
patrykmysiorski <patryk.mysiorski@gmail.com>
2020-06-26 15:44:56 +0200

    add add and delete products functionality

--- src/components/admin/Products.tsx
+++ src/components/admin/Products.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/standardFont.css'
+import { deleteProduct } from "../../actions/productActions";
+import { useDispatch, useSelector } from "react-redux";
+import { Product } from "../../types/productsTypes";
+import AddProductForm from "./AddProductForm";
 
-const App: React.FC = () =>
-    <div className="font-white">
-        <h1>Admin Products works!</h1>
-    </div>
+const Products: React.FC = (props: any) => {
+    const dispatch = useDispatch();
+    const [addClicked, setAddClicked] = useState(false);
+    const products: Product[] = useSelector((state: any) => state.productsReducer.products)
+    const onDelete = (id: number) => {
+        dispatch(deleteProduct(id));
+    }
+    const onAddButtonClicked = () => {
+        setAddClicked(!addClicked);
+    }
 
-export default App;
+    return (
+        <div className="center">
+            <button onClick={onAddButtonClicked}>Add product</button>
+            {addClicked && (
+                <AddProductForm/>
+            )}
+            {products.map(listItem =>
+                <div key={listItem.id}>
+                    <div>{listItem.title}</div>
+                    <button onClick={() => onDelete(listItem.id)}>delete</button>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Products;
